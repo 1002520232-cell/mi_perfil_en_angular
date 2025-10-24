c1a960c57ad961124f72af97b40c4c11657f2a9a
Juan David Aguila <1002520232@unajma.edu.pe>
2025-10-23 19:47:50 -0500

    *ngIf, (click), [ngClass] y [ngStyle] usado

--- src/app/componentes/proyectos/proyectos.ts
+++ src/app/componentes/proyectos/proyectos.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-proyectos',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './proyectos.html',
   styleUrl: './proyectos.css'
 })
 export class Proyectos {
-
+  proyecto1Seleccionado = false;
+  proyecto2Seleccionado = false;
+  proyecto3Seleccionado = false;
 }
